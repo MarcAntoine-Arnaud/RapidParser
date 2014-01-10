3c14a7057b66c7d587e10a2149da4dff65807662
Valentin Noel <van@mikrosimage.eu>
2014-01-10 16:59:14 +0100

    clean in Deserializer.hpp

--- src/Deserializer.hpp
+++ src/Deserializer.hpp
@@ -13,11 +13,6 @@ public:
 	{
 	}
 	
-	// virtual void get( const char* key, const std::string& data, const std::vector< char* >& path ) = 0;
-	// virtual void get( const char* key, const size_t data, const std::vector< char* >& path ) = 0;
-	// virtual void get( const char* key, const int data, const std::vector< char* >& path ) = 0;
-	// virtual void get( const char* key, const double data, const std::vector< char* >& path ) = 0;
-	
 	virtual bool get( const char* key, std::string& data, const std::vector< std::pair< char*, size_t > >& path ) = 0;
 	virtual bool get( const char* key, int& data,         const std::vector< std::pair< char*, size_t > >& path ) = 0;
 	virtual bool get( const char* key, double& data,      const std::vector< std::pair< char*, size_t > >& path ) = 0;
@@ -25,7 +20,6 @@ public:
 
 	virtual void load( const std::string& string ) = 0;
 
-	// virtual std::string get() = 0;
 };
 
 }
